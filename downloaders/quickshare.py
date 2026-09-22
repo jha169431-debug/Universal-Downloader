95dcc62d7af99e8695c5f53762233190159ae4c1
jha169431-debug <jha169431@gmail.com>
2026-09-22 19:35:06 +0545

    Show Quick Share source in unified UI

--- downloaders/quickshare.py
+++ downloaders/quickshare.py
@@ -1,5 +1,6 @@
 import json
 import re
+
 import requests
 
 from downloaders.direct import DirectDownloader
@@ -8,7 +9,9 @@ from downloaders.direct import DirectDownloader
 class QuickShare:
     def __init__(self):
         self.session = requests.Session()
-        self.downloader = DirectDownloader()
+        self.downloader = DirectDownloader(
+            source_name="Samsung Quick Share"
+        )
 
     def download(self, url):
         html = self.session.get(
@@ -19,20 +22,22 @@ class QuickShare:
                     "AppleWebKit/537.36 (KHTML, like Gecko) "
                     "Chrome/138.0 Mobile Safari/537.36"
                 )
-            }
+            },
+            timeout=30,
         ).text
 
         match = re.search(
             r'options\.sharedatacontents\s*=\s*JSON\.parse\(\'(.*?)\'\);',
             html,
-            re.DOTALL
+            re.DOTALL,
         )
 
         if not match:
-            raise Exception("Unable to locate Quick Share data.")
+            raise Exception(
+                "Unable to locate Quick Share data."
+            )
 
         data = json.loads(match.group(1))
-
         real_url = data[0]["original"]
 
         self.downloader.download(real_url)
